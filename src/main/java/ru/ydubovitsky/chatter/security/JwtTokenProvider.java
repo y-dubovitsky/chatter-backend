39733142e03fc5566760a7367eba26faa78cf391
user <y.dubovitsky@gmail.com>
2021-09-22 14:57:27 +0300

    added a component for generating, validating and parsing a web token

---
+++ src/main/java/ru/ydubovitsky/chatter/security/JwtTokenProvider.java
@@ -0,0 +1,62 @@
+package ru.ydubovitsky.chatter.security;
+
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import ru.ydubovitsky.chatter.entity.User;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider { // Этот класс отвечает за создание Токена
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(JwtTokenProvider.class);
+
+    public String generateWebToken(Authentication authentication) { //FIXME Что такое Authentication? Как я помню, это данные о аутентифицированном пользователе
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date dateExpiration = new Date(now.getTime() + SecurityConstants.EXPIRATION_TIME);
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claims = new HashMap<>(); // Объект, из которого будет генерироваться токен
+
+        claims.put("id", userId);
+        claims.put("username", user.getUsername());
+        claims.put("lastname", user.getLastname());
+        claims.put("email", user.getEmail());
+
+        return Jwts.builder() // Генерируем ТОКЕН
+                .setSubject(userId)
+                .addClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(dateExpiration)
+                .signWith(SignatureAlgorithm.ES512, SecurityConstants.SECRET)
+                .compact();
+    }
+
+    public boolean validateToken(String token) { // Валидируем токен!
+        try {
+            Jwts.parser()
+                    .setSigningKey(SecurityConstants.SECRET)
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException | MalformedJwtException | ExpiredJwtException | UnsupportedJwtException | IllegalArgumentException e) {
+            LOGGER.error(String.valueOf(e));
+            return false;
+        }
+    }
+
+    public Long getUserIdFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+
+        String userId = (String) claims.get("id");
+        return Long.parseLong(userId);
+    }
+}
---
+++ src/main/java/ru/ydubovitsky/chatter/security/SecurityConstants.java
@@ -0,0 +1,12 @@
+package ru.ydubovitsky.chatter.security;
+
+public class SecurityConstants {
+
+    public static final String SIGN_UP_URL = "/api/auth/**";
+    public static final String SECRET = "SecretKey";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization ";
+    public static final String CONTENT_TYPE = "application/json ";
+    public static final long EXPIRATION_TIME = 600_000; //10 min
+
+}

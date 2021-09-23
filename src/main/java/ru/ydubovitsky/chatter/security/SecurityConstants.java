90b161ed3496af38e5466405859cc3547cbb6555
user <y.dubovitsky@gmail.com>
2021-09-23 14:38:23 +0300

    increased the lifetime of the token

--- src/main/java/ru/ydubovitsky/chatter/security/SecurityConstants.java
+++ src/main/java/ru/ydubovitsky/chatter/security/SecurityConstants.java
@@ -7,6 +7,6 @@ public class SecurityConstants {
     public static final String TOKEN_PREFIX = "Bearer "; //! Должен быть пробел
     public static final String HEADER_STRING = "Authorization ";
     public static final String CONTENT_TYPE = "application/json ";
-    public static final long EXPIRATION_TIME = 600_000; //10 min
+    public static final long EXPIRATION_TIME = 6_000_000; //100 min
 
 }
